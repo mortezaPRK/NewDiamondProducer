bed7836ee4054fa538cd845d5d9bb753af977fd0
mortzprk <mortz.prk@gmail.com>
2020-12-13 19:24:27 +0330

    add Topology

--- Geometry/VeryForwardGeometry/interface/CTPPSDiamondTopology.h
+++ Geometry/VeryForwardGeometry/interface/CTPPSDiamondTopology.h
@@ -1,24 +1,49 @@
 #ifndef Geometry_VeryForwardGeometry_CTPPSDiamondTopology_h
 #define Geometry_VeryForwardGeometry_CTPPSDiamondTopology_h
 
+#include <numeric>
+
 class CTPPSDiamondTopology {
 public:
   CTPPSDiamondTopology() = default;
   ~CTPPSDiamondTopology() = default;
 
+  static constexpr double active_edge_sigma_ = 0.02;
   static constexpr double thickness_ = 0.23;
-  static constexpr double simX_width_ = 16.6;
-  static constexpr double simY_width_ = 24.4;
-  static constexpr double dead_edge_width_ = 200E-3;
-  static constexpr double dead_part_width_ = 200E-3;
+  static constexpr double sensor_outer_edge_ = 0.15;
+  static constexpr double sensor_inner_edge_ = 0.1;
+  static constexpr double x_width_ = 4.2;
+  static constexpr double y_each_width_[8] = {
+      0.7,
+      0.83,
+      1.02,
+      1.35,
+      1.75,
+      2.35,
+      4.2,
+      4.2,
+  };
+
+  inline double yWidth() const {
+    int numberOfSensors = sizeof(y_each_width_);
+    double w = std::accumulate(y_each_width_, numberOfSensors, 0);
+    return w + ((numberOfSensors - 1) * sensor_inner_edge_) + 2 * sensor_outer_edge_;
+  }
+
+  inline double xWidth() const { return x_width_ + 2 * sensor_outer_edge_; }
 
   static bool isValidHit(float xLocalCoordinate, float yLocalCoordinate) {
-    // TODO: check if hit is actually inside our detector
+    if (xLocalCoordinate > xWidth() / 2. || xLocalCoordinate < -xWidth() / 2. ||) {
+      return false
+    }
+    if (yLocalCoordinate > yWidth() / 2. || yLocalCoordinate < -yWidth() / 2. ||) {
+      return false
+    }
+    return true
   }
 
   CTPPSDiamondIndices indices_;
 
-  //  TODO: check https://github.com/CTPPS/cmssw/blob/FullSim_SimG4CMS-PPS/SimG4CMS/PPS/src/PPSDiamondOrganization.cc
   //     ________
   //    |        |
   //    |        |
@@ -41,33 +66,50 @@ public:
   // 1  |________|
   class PartInfo {
   public:
-    PartInfo(double lower_simX_border,
-              double higher_simX_border,
-              double lower_simY_border,
-              double higher_simY_border,
-              double eff_factor)
-        : lower_simX_border_(lower_simX_border),
-          higher_simX_border_(higher_simX_border),
-          lower_simY_border_(lower_simY_border),
-          higher_simY_border_(higher_simY_border),
-          eff_factor_(eff_factor) {}
+    PartInfo(double lower_y_border,
+             double higher_y_border,
+             double eff_factor,
+             int part_index)
+        : lower_y_border_(lower_y_border),
+          higher_y_border_(higher_y_border),
+          eff_factor_(eff_factor)
+          part_index_(part_index) {}
 
-    inline double higherSimXBorder() const { return higher_simX_border_; }
-    inline double lowerSimXBorder() const { return lower_simX_border_; }
-    inline double higherSimYBorder() const { return higher_simY_border_; }
-    inline double lowerSimYBorder() const { return lower_simY_border_; }
+    inline double higherYBorder() const { return higher_y_border_; }
+    inline double lowerYBorder() const { return lower_y_border_; }
     inline double effFactor() const { return eff_factor_; }
-    inline double partIndex() const { return eff_factor_; }
-    
+    inline double partIndex() const { return part_index_; }
 
   private:
-    double lower_simX_border_;
-    double higher_simX_border_;
-    double lower_simY_border_;
-    double higher_simY_border_;
+    double lower_y_border_;
+    double higher_y_border_;
     double eff_factor_;
+    int part_index_;
   };
 
+private:
+  double active_edge_x_;
+  double active_edge_y_;
+
+  inline double activeEdgeFactor(double x, double y) const {
+    const double inv_sigma = 1. / active_edge_sigma_;  // precaching
+    const double topEdgeFactor = std::erf(-distanceFromTopActiveEdge(x, y) * inv_sigma) * 0.5 + 0.5;
+    const double bottomEdgeFactor = std::erf(-distanceFromBottomActiveEdge(x, y) * inv_sigma) * 0.5 + 0.5;
+    const double rightEdgeFactor = std::erf(-distanceFromRightActiveEdge(x, y) * inv_sigma) * 0.5 + 0.5;
+    const double leftEdgeFactor = std::erf(-distanceFromLeftActiveEdge(x, y) * inv_sigma) * 0.5 + 0.5;
+
+    const double aEF = topEdgeFactor * bottomEdgeFactor * rightEdgeFactor * leftEdgeFactor;
+
+    if (aEF > 1.)
+      throw cms::Exception("CTPPSDiamondTopology") << " active edge factor > 1";
+
+    return aEF;
+  }
+
+  inline double distanceFromTopActiveEdge(double x, double y) const { return (y - active_edge_y_); }
+  inline double distanceFromBottomActiveEdge(double x, double y) const { return (-y - active_edge_y_); }
+  inline double distanceFromRightActiveEdge(double x, double y) const { return (x - active_edge_x_); }
+  inline double distanceFromLeftActiveEdge(double x, double y) const { return (-x - active_edge_x_); }
 };
 
 #endif
